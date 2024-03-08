50340a9734f3a216a628009fc330cbee86f16cc8
Victor Matheron <victor.matheron@gmail.com>
2024-03-08 13:59:49 +0100

    Clean Code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,29 +10,31 @@ class Home extends StatelessWidget {
     var service = PostService();
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("Albums app"),
-        ),
-        body: FutureBuilder(
-          future: service.fetchPosts(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              // Si l'on récupère la data correctement, afficher un ListView
-              var posts = snapshot.data!;
-              return ListView.builder(
-                itemCount: posts.length,
-                itemBuilder: (context, index) =>
-                    _listElement(context, posts[index]),
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-
-            // Par défaut, afficher un Spinner
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Albums app"),
+      ),
+      body: FutureBuilder(
+        future: PostService().fetchPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // Si notre Future renvoie la data correctement, afficher un ListView
+            var posts = snapshot.data!;
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) =>
+                  _listElement(context, posts[index]),
+            );
+          } else if (snapshot.hasError) {
+            // Si notre Future renvoie une erreur, afficher l'erreur (?)
+            return Text('${snapshot.error}');
+          } else {
+            // Par défaut (en attendant), afficher un spinner
             return const CircularProgressIndicator();
-          },
-        ));
+          }
+        },
+      ),
+    );
   }
 
   _listElement(context, Post post) {
--- lib/service/post_service.dart
+++ lib/service/post_service.dart
@@ -4,18 +4,9 @@ import 'package:fetch_data_app/model/post.dart';
 import 'package:http/http.dart' as http;
 
 class PostService {
-  List<Post> parsePosts(String responseBody) {
-    // Parse la réponse en tant que liste de dynamic à l'aide de jsonDecode
-    final List<dynamic> data = jsonDecode(responseBody);
-    // Effectue un mapping de chaque élément 'dynamic' de notre List
-    // Map un 'Post' grâce au constructeur .fromJson
-    // Retourne une List<Post> avec .toList();
-    return data.map<Post>((json) => Post.fromJson(json)).toList();
-  }
-
   Future<List<Post>> fetchPosts() async {
-    final response = await http
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/'));
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/');
+    final response = await http.get(url);
 
     // Si l'on a un HTTP 200, on parse la réponse de notre webservice
     if (response.statusCode == 200) {
@@ -23,5 +14,15 @@ class PostService {
     } else {
       throw Exception('Failed to load album');
     }
+  }
+
+  List<Post> parsePosts(String responseBody) {
+    // Parse la réponse en tant que liste de dynamic à l'aide de jsonDecode
+    final List<dynamic> data = jsonDecode(responseBody);
+
+    // Effectue un mapping de chaque élément 'dynamic' de notre List
+    // Map chaque élément en 'Post' grâce au constructeur .fromJson
+    // Retourne une List<Post> avec .toList();
+    return data.map<Post>((json) => Post.fromJson(json)).toList();
   }
 }
